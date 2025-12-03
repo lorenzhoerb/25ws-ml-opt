0daa8c27673cc2c9d597c375c9118719f68e3d95
Lorenz Hörburger <lorenz.hoerburger@gmx.at>
2025-12-03 15:58:50 +0100

    implement python mzn solver wrapper

---
+++ asg01-golfer/src/mzn_solver.py
@@ -0,0 +1,39 @@
+from minizinc import Model, Solver, Instance
+from datetime import timedelta
+import numpy as np
+
+
+class MiniZincGolferSolver:
+    '''
+    Solver for the Social Golfer Problem using minizinc.
+    Minizinc must be installed locally and the solver must be available.
+    List available solvers using `minizinc --sovlers`
+    '''
+
+    def __init__(
+            self, 
+            model: str = "./golfers.mzn", 
+            solver: str = "gecode",
+            time_limit: timedelta = None
+        ) -> None :
+
+        self.model = Model(model)
+        self.solver = Solver.lookup(solver)
+        self.time_limit = time_limit
+
+    def solve(self, n_groups: int, n_per_group: int, n_rounds: int) -> np.ndarray:
+        assert n_groups  > 0, "n_groups must be > 0"
+        assert n_per_group  > 0, "n_per_group must be > 0"
+        assert n_rounds  > 0, "n_rounds must be > 0"
+
+        instance = Instance(self.solver, self.model)
+        instance["n_groups"] = n_groups
+        instance["n_per_group"] = n_per_group
+        instance["n_rounds"] = n_rounds
+
+        result = instance.solve(time_limit=self.time_limit)
+        schedule = result["schedule"]
+        if not schedule:
+            return None
+        
+        return np.array(schedule)
